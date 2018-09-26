97c149e55533001715e94ebefc43a4e1605b6b2f
leonard186 <ballok.leonard@yahoo.com>
2018-09-26 14:20:13 +0100

    code refactored, new board added  and buttons

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,82 +1,186 @@
-//Global variables
+/*****************************************************
+ * Global variables
+ *****************************************************/
+let audio = new Audio('./resources/sounds/WON.mp3');
+
+/*****************************************************
+ * declare arrays
+ *****************************************************/const game2 = [].slice.call(document.querySelectorAll('.game .tile'));
+
+    let array8 = [];
+    let array16 = [];
+
+    /*****************************************************
+     * general purpose functions
+     *****************************************************/
 
 
 //check if element has class --> returns true or false
-function hasClass(element, className) {
-    return element.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(element.className);
-};
+    function hasClass(element, className) {
+        return element.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(element.className);
+    };
 
-//Get DOM elements
-const dom = {
-    tileGame1: document.querySelectorAll('.grid .tile'),
-    tileGame1Img: document.querySelectorAll('.tile img'),
-    game1: document.getElementById('game1'),
-    game1Img: this.game1.getElementsByTagName('img')
-};
+    /*****************************************************
+     * Generate Boards
+     *****************************************************/
+    generateBoardTiles(8);
+    function generateBoard(domCollection, callback, arr, gameNumber) {
+        domCollection.forEach(element => {
+            callback(element, arr, gameNumber);
+        });
+    };
 
-//const addHtmlElement = `<img src="./resources/images/animals/game1/${number}.png`;
+    function renderBoard8(element, arr, gameNumber) {
+        if (arr.length > 0) {
+            const holder = arr.pop();
+            element.dataset.index = holder;
+            element.innerHTML += `<img class="back" src="./resources/images/cover.png">`;
+            element.innerHTML += `<img class="front" src="./resources/images/animals/game${gameNumber}/${holder}.png">`
+        }
+    };
 
-const game1Dom = [].slice.call(dom.tileGame1);
-const game1ImgDom = [].slice.call(dom.game1Img);
+    function initiate(targetDomElement, arr, gameNumber, numOfTiles) {
+        clearBoard(targetDomElement);
+        resetValues();
+        arr = random(numOfTiles, arr);
+        generateBoard(targetDomElement, renderBoard8, arr, gameNumber);
+        targetDomElement.forEach(el => el.addEventListener('click', gameProcess));
+    }
+
+    function clearBoard(gameDom) {
+        gameDom.forEach(e => {
+            e.removeAttribute('data-index');
+            if (hasClass(e, 'flip')) {
+                e.classList.remove('flip');
+            }
+            while (e.firstChild) {
+                e.removeChild(e.firstChild);
+            }
+        })
+    }
+
+function generateBoardTiles(num) {
+    num = num * 2;
+    while (num > 0) {
+        document.getElementById('game1').innerHTML += '<div class="tile"></div>';
+        num--;
+    }
+}
+
+/***************************************************
+ /////////////////////////// not in use
+function generateBoardTiles(arr) {
+        //document.getElementById('game').innerHTML = '';
+        let c = arr.length;
+        while (c > 0) {
+            document.getElementById('game').innerHTML += '<div class="tile"></div>';
+            c--;
+        }
+    }
+
+*///////////////////////////////////////////
 
 
-const iterateDomCollection = function(domCollection, callback) {
-    domCollection.forEach(element => {
-        callback(element);
-    });
-};
-
-let array8 = [];
-array8 = random(8, array8);
+/*****************************************************
+ * Generate Random array
+ *****************************************************/
 
 //this function returns a random array double the length of the initial input
 //use like this: random(8, arrayToFillWithRandomNumbers)
-function random(num, newArray) {
-    newArray = [];
-
-    for (let i = 0; i < num; i++) {
-        newArray.push(i + 1);
-        newArray.push(i + 1);
-    }
-    let counter = newArray.length;
-    while (counter > 0) {
-        let index = Math.floor(Math.random() * counter);
-        counter--;
-        let temp = newArray[counter];
-        newArray[counter] = newArray[index];
-        newArray[index] = temp;
-    }
-    return newArray
-}
-
-
-function renderGame1(element) {
-    if (array8.length > 0) {
-        const holder = array8.pop();
-        element.dataset.index = holder;
-        element.innerHTML = `<img class="hidden" src="./resources/images/animals/game1/${holder}.png">`
-    }
-}
-
-iterateDomCollection(game1Dom, renderGame1);
-
-//game logic
-
-const flipGame1 = [0, 0];
-
-game1Dom.forEach(function(element) {
-    element.addEventListener('click', function() {
-        if (flipGame1[0] !== 0) {
-            flipGame1[0] = Number(this.getAttribute('data-index'));
-            if (hasClass(element.firstChild, 'hidden')) {
-                element.firstChild.classList.remove('hidden');
-                element.firstChild.classList.add('show');
-
-            } else if (hasClass(element.firstChild, 'show')) {
-                element.firstChild.classList.remove('show');
-                element.firstChild.classList.add('hidden');
-            }
+    function random(num, newArray) {
+        newArray = [];
+        for (let i = 0; i < num; i++) {
+            newArray.push(i + 1);
+            newArray.push(i + 1);
         }
-        console.log(this.getAttribute('data-index'))
-    })
-});
+        let counter = newArray.length;
+        while (counter > 0) {
+            let index = Math.floor(Math.random() * counter);
+            counter--;
+            let temp = newArray[counter];
+            newArray[counter] = newArray[index];
+            newArray[index] = temp;
+        }
+        return newArray
+    }
+
+    /*****************************************************
+     * Generate Boards
+     *****************************************************/
+
+//game logic variables
+    let flipped = false;
+    let first, second;
+    let lock = false;
+    let counter = 0;
+
+
+    function gameProcess() {
+
+        if (lock) return;
+        if (this === first) return;
+
+        this.classList.toggle('flip');
+
+        if (!flipped) {
+            flipped = true;
+            first = this;
+            return
+        }
+        flipped = false;
+        second = this;
+        console.log(first.dataset.index);
+        console.log(second.dataset.index);
+        checkValidity();
+    }
+
+    function checkValidity() {
+        first.dataset.index === second.dataset.index ?
+            disable() : flipBack();
+    }
+
+    function disable() {
+        let check = [... document.querySelectorAll('.game .tile')].length / 2;
+        first.removeEventListener('click', gameProcess);
+        second.removeEventListener('click', gameProcess);
+        counter++;
+        if (counter > (check - 1)) {
+            audio.play();
+        }
+        resetValues();
+        console.log('counter:' + counter);
+    }
+
+    function flipBack() {
+        lock = true;
+        setTimeout(() => {
+            first.classList.toggle('flip');
+            second.classList.toggle('flip');
+            resetValues();
+        }, 550);
+
+    }
+
+    function resetValues() {
+        [flipped, lock] = [false, false];
+        [first, second] = [null, null];
+    }
+
+/*****************************************************
+ * Initiate boards
+ *****************************************************/
+
+    const game1 = [].slice.call(document.querySelectorAll('.game .tile'));
+
+    //const game2 = [].slice.call(document.querySelectorAll('.game')[1].querySelectorAll('.tile'));
+    document.getElementById('game1-btn').addEventListener('click', initiate.bind(this, game1, array8, 1, 8, false));
+    document.getElementById('game2-btn').addEventListener('click', initiate.bind(this, game1, array16, 2, 8, false));
+
+
+
+
+
+
+
+
+
